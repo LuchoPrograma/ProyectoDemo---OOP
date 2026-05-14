62a44e38dd9dacc0711448b72fbf5ae5ec632d89
LuchoPrograma <lucianofedericci@gmail.com>
2026-05-13 22:16:21 -0300

    fix: correcciones a LocalidadController y LocalidadServiceImpl

--- src/main/java/com/example/proyectoDemo/services/LocalidadServiceImpl.java
+++ src/main/java/com/example/proyectoDemo/services/LocalidadServiceImpl.java
@@ -4,9 +4,11 @@ import com.example.proyectoDemo.entities.Localidad;
 import com.example.proyectoDemo.repositories.BaseRepository;
 import com.example.proyectoDemo.repositories.LocalidadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class LocalidadServiceImpl extends BaseServiceImpl<Localidad,Long> implements LocalidadService {
 
     @Autowired
